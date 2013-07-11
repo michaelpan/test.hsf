1f71b0eb01fb24b3f0c074ace46a348bb81c504a
Michael Pan <pangl@threemickey.com>
2013-07-11 14:21:48 +0800

    Use system parameter to set path of taobao_hsf.sar

--- test.hsf.client/src/main/java/com/mitch3/test/hsf/client/Client.java
+++ test.hsf.client/src/main/java/com/mitch3/test/hsf/client/Client.java
@@ -7,16 +7,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.mitch3.test.hsf.api.IHSFTestInterface;
 import com.taobao.hsf.standalone.HSFEasyStarter;
 
-
+/**
+ * 测试客户端，需要指定hsfSarDir系统参数。
+ * 即指定JVM的参数：-DhsfSarDir，值为taobao_hsf.sar的父目录地址
+ * @author guolin.pangl
+ *
+ */
 public class Client {
 	
+	public static final String HSF_SAR_DIR = "hsfSarDir";
+	
 	public static void main(String[] args) throws Exception{
-		File root = new File(Client.class.getResource("/").getFile());
-		File hsfSarPath = new File(root, "ext");
+		
+		String sarDir = System.getProperty(HSF_SAR_DIR);
+		if (sarDir == null) {
+			System.err.println("Must set system parameter hsfSarDir to parent directory path of 'taobao_hsf.sar'");
+			System.err.println("You can set this by jvm param -DhsfSarDir.");
+			System.err.println("System now exist.");
+			System.exit(-1);
+		}
 		String applicationContextFile = "applicationContext.xml";
 		String beanName = "hsfTestBean";
 		
-		HSFEasyStarter.startFromPath(hsfSarPath.getAbsolutePath());
+		HSFEasyStarter.startFromPath(sarDir);
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(applicationContextFile);
 		
 		IHSFTestInterface test = (IHSFTestInterface) context.getBean(beanName);
